54fd8fc2a4b964eee359b23eae973cc31c3251b9
Tim Wood <lexicalscope@gmail.com>
2011-10-24 22:35:45 +0100

    fixup proguard configuration

---
+++ jewelcli-packagingtest/src/test/java/com/lexicalscope/jewelcli/JewelCliPackagingTest/TestCliParsing.java
@@ -0,0 +1,51 @@
+package com.lexicalscope.jewelcli.JewelCliPackagingTest;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import uk.co.flamingpenguin.jewel.cli.ArgumentValidationException;
+import uk.co.flamingpenguin.jewel.cli.CliFactory;
+import uk.co.flamingpenguin.jewel.cli.Option;
+
+/*
+ * Copyright 2011 Tim Wood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+public class TestCliParsing {
+    interface MyOptions
+    {
+        @Option Integer getMyOption();
+    }
+
+    @Test public void parsingWorks() throws ArgumentValidationException
+    {
+        final MyOptions parsedArguments =
+                CliFactory.parseArguments(MyOptions.class, new String[] { "--myOption", "14" });
+        AssertJUnit.assertEquals((Integer) 14, parsedArguments.getMyOption());
+    }
+
+    @Test public void validationWorks()
+    {
+        try {
+
+            CliFactory.parseArguments(MyOptions.class, new String[] {});
+            AssertJUnit.fail();
+        } catch (final ArgumentValidationException e) {
+            AssertJUnit.assertEquals(
+                    e.getValidationErrors().get(0).getErrorType(),
+                    ArgumentValidationException.ValidationError.ErrorType.MissingOption);
+        }
+    }
+}
