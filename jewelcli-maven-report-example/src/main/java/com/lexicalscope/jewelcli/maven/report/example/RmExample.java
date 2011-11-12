561b9a0fa5a302a6a29f1dd659d83443c6b55d85
Tim Wood <lexicalscope@gmail.com>
2011-11-12 17:29:28 +0000

    maven plugin for generating report on the Command line interface of a project

---
+++ jewelcli-maven-report-example/src/main/java/com/lexicalscope/jewelcli/maven/report/example/RmExample.java
@@ -0,0 +1,27 @@
+package com.lexicalscope.jewelcli.maven.report.example;
+
+import java.io.File;
+import java.util.List;
+
+import uk.co.flamingpenguin.jewel.cli.CommandLineInterface;
+import uk.co.flamingpenguin.jewel.cli.Option;
+import uk.co.flamingpenguin.jewel.cli.Unparsed;
+
+@CommandLineInterface(application = "rm") public interface RmExample
+{
+    @Option(shortName = "d", longName = "directory", description = "unlink FILE, even if it is a non-empty directory (super-user only)") boolean isRemoveNonEmptyDirectory();
+
+    @Option(shortName = "f", description = "ignore nonexistent files, never prompt") boolean isForce();
+
+    @Option(shortName = "i", description = "prompt before any removal") boolean isInteractive();
+
+    @Option(shortName = { "r", "R" }, description = "remove the contents of directories recursively") boolean isRecursive();
+
+    @Option(shortName = "v", description = "explain what is being done") boolean isVerbose();
+
+    @Option(description = "display this help and exit") boolean isHelp();
+
+    @Option(description = "output version information and exit") boolean isVersion();
+
+    @Unparsed(name = "FILE") List<File> getFiles();
+}
---
+++ jewelcli-maven-report-plugin/src/main/java/com/lexicalscope/jewelcli/maven/report/JewelCliReportMojo.java
@@ -0,0 +1,186 @@
+package com.lexicalscope.jewelcli.maven.report;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.doxia.sink.Sink;
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+
+import uk.co.flamingpenguin.jewel.cli.CliFactory;
+
+/**
+ * Goal which produces a CLI help page for you maven site
+ * 
+ * @author Tim Wood
+ * @goal report
+ * @execute phase="compile" lifecycle="jewelcli"
+ * @requiresDependencyResolution compile
+ * @requiresProject true
+ * @requiresReports true
+ */
+public class JewelCliReportMojo
+        extends AbstractMavenReport
+{
+    /**
+     * Directory where reports will go.
+     * 
+     * @parameter expression="${project.reporting.outputDirectory}"
+     * @required
+     * @readonly
+     */
+    private String outputDirectory;
+
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    /**
+     * @component
+     * @required
+     * @readonly
+     */
+    private Renderer siteRenderer;
+
+    /**
+     * The interfaces to output
+     * 
+     * @parameter
+     * @required
+     */
+    private String[] interfaces;
+
+    @Override public String getDescription(final Locale locale) {
+        return getBundle(locale).getString("report.jewelcli.description");
+    }
+
+    @Override public String getName(final Locale locale) {
+        return getBundle(locale).getString("report.jewelcli.name");
+    }
+
+    String[] getInterfaces() {
+        return interfaces;
+    }
+
+    void setInterfaces(final String[] interfaces) {
+        this.interfaces = interfaces;
+    }
+
+    @Override public String getOutputName() {
+        return "command-line-interface";
+    }
+
+    @Override protected void executeReport(final Locale locale) throws MavenReportException {
+        final ClassLoader classLoader = getClassLoader();
+        final Class<?> interfaceClass = interfaceClass(classLoader);
+
+        final String helpMessage = CliFactory.createCli(interfaceClass).getHelpMessage();
+        getLog().debug("cli interface with  " + helpMessage);
+
+        final Sink sink = getSink();
+        sink.head();
+        sink.title();
+        sink.text(getName(locale));
+        sink.title_();
+        sink.head_();
+
+        sink.body();
+        sink.section1();
+
+        sink.sectionTitle1();
+        sink.text(getBundle(locale).getString("report.jewelcli.title") + " " + getProject().getName());
+        sink.sectionTitle1_();
+        sink.lineBreak();
+        sink.lineBreak();
+
+        sink.text("Command line interface for " + " " + getProject().getName());
+        sink.lineBreak();
+
+        final Scanner scanner = new Scanner(helpMessage);
+        while (scanner.hasNextLine()) {
+            sink.lineBreak();
+            sink.text(scanner.nextLine());
+        }
+
+        sink.lineBreak();
+        sink.section1_();
+        sink.body_();
+        sink.flush();
+        sink.close();
+    }
+
+    private Class<?> interfaceClass(final ClassLoader classLoader) throws MavenReportException {
+        final String interfac3 = interfaces[0];
+        getLog().debug("generating command line interface report for " + interfac3);
+        try {
+            return classLoader.loadClass(interfac3);
+        } catch (final ClassNotFoundException e) {
+            throw new MavenReportException("Unable to load interface class " + interfac3, e);
+        }
+    }
+    @Override protected String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    @Override protected MavenProject getProject() {
+        return project;
+    }
+
+    @Override protected Renderer getSiteRenderer() {
+        return siteRenderer;
+    }
+
+    private ResourceBundle getBundle(final Locale locale)
+    {
+        return ResourceBundle.getBundle("jewelcli-report", locale, this.getClass().getClassLoader());
+    }
+
+    private ClassLoader getClassLoader() throws MavenReportException {
+        final List<URL> urls = new ArrayList<URL>();
+        try {
+            for (final Object object : project.getCompileClasspathElements()) {
+                final String path = (String) object;
+                final URL url = new File(path).toURL();
+
+                getLog().debug("adding classpath element " + url);
+
+                urls.add(url);
+            }
+        } catch (final MalformedURLException e) {
+            throw new MavenReportException("Unable to load command line interface class", e);
+        } catch (final DependencyResolutionRequiredException e) {
+            throw new MavenReportException("Unable to resolve dependencies of project", e);
+        }
+
+        return new URLClassLoader(urls.toArray(new URL[] {}), getClass().getClassLoader());
+    }
+}
